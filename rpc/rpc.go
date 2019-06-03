770f1a073ff7adffcaaece4847b2609788d84232
rwifeng <rw.ifeng@gmail.com>
2019-06-03 09:42:30 +0800

    seq read do not reconstrut
    Former-commit-id: 6f3a44c18c37c6f5dcdac6d31370e02595cba665

--- rpc/rpc.go
+++ rpc/rpc.go
@@ -393,105 +393,110 @@ func (c *Client) Download(fileHash string) (rc io.ReadCloser, metaAll metafile.M
 	}
 
 	dataShards := int(meta.MetaHeader.DataShards)
-	parShards := int(meta.MetaHeader.ParShards)
-	shards := dataShards + parShards
-	rcs := make([]io.ReadCloser, shards)
-	brokenShards := 0
-	for i := 0; i < shards; i++ {
+	_, _, shardSize := file.BlockCount(meta.FSize)
+	lastShardSize := meta.FSize - int64(dataShards-1)*shardSize
+	rcs := make([]io.ReadCloser, dataShards)
+	retryTimes := 0
+	var partSize int64 = 0
+	for i := 0; i < dataShards; i++ {
 		blockInfo := blocksInfo[i]
-		node, e := c.GetNodeClient(blockInfo.PeerId)
+		nodes, e := c.GetNodeClients(blockInfo.PeerId)
 		if e != nil {
 			err = e
-			brokenShards++
-			rcs[i] = nil
 			log.Printf("GetNodeClient error: %s, node id: %s, block hash: %s \n", blockInfo.PeerId, blockInfo.BlockHash, err)
-			continue
+			return
 		}
 
-		var rc1 io.ReadCloser
-		rc1, err = ReadAt(node.Client, blockInfo.BlockHash, int64(meta.Len()), 0)
-		if err != nil {
-			rcs[i] = nil
-			brokenShards++
-		} else {
-			rcs[i] = rc1
+		if i == dataShards-1 {
+			partSize = lastShardSize
 		}
+
+		node := nodes[0]
+	lazyTry:
+		rc1, e := ReadAt(node.Client, blockInfo.BlockHash, int64(meta.Len()), partSize)
+		if e != nil {
+			err = e
+			if retryTimes < 3 {
+				log.Printf("read failed, node id: %s, block hash: %s, error: %#v , retryTimes: %d, retrying\n", blockInfo.PeerId, blockInfo.BlockHash, err, retryTimes)
+				node = getRandonNode(nodes)
+				retryTimes++
+				goto lazyTry
+			}
+			return
+		}
+
+		rcs[i] = rc1
 		log.Printf("read block, node id: %s, Block Hash: %s, error: %s \n", node.Id, blockInfo.BlockHash, err)
-
-		if i == dataShards-1 && brokenShards == 0 {
-			rcs = rcs[:dataShards]
-			break
-		}
 	}
 
-	if brokenShards != 0 {
-		log.Printf("shards num: %d broken shards: %d \n", shards, brokenShards)
-		mgr, err1 := file.NewBlockMgr(dataShards, parShards)
-		if err1 != nil {
-			err = err1
-			return
-		}
-
-		wg := &sync.WaitGroup{}
-		wg.Add(shards - brokenShards)
-		wtrs := make([]io.Writer, shards)
-		for i := range rcs {
-			fh, err1 := ioutil.TempFile("", meta.FName+"."+strconv.Itoa(i))
-			if err1 != nil {
-				err = err1
-				return
-			}
-			if rcs[i] != nil {
-				go func() {
-					defer wg.Done()
-					_, err = io.Copy(fh, rcs[i])
-					if err != nil {
-						return
-					}
-				}()
-			} else {
-				wtrs[i] = fh
-			}
-		}
-		wg.Wait()
-		rds := make([]io.Reader, shards)
-		for i := range rcs {
-			if rcs[i] != nil {
-				rcs[i].Close()
-
-				fh, err1 := os.Open(meta.FName + "." + strconv.Itoa(i))
-				if err1 != nil {
-					err = err1
-					return
-				}
-				rds[i] = fh
-			} else {
-				rds[i] = nil
-			}
-		}
-		err = mgr.Reconstruct(rds, wtrs)
-		if err != nil {
-			return
-		}
-
-		for i := range rcs {
-			if rcs[i] == nil {
-				fh := wtrs[i].(*os.File)
-				fh.Seek(0, 0)
-				rcs[i] = fh
-			} else {
-				fh := rds[i].(*os.File)
-				fh.Seek(0, 0)
-				rcs[i] = fh
-			}
-
-			if i >= dataShards {
-				rcs[i].Close()
-			}
-		}
-
-		rcs = rcs[:dataShards]
-	}
+	//if brokenShards != 0 {
+	//	log.Printf("shards num: %d broken shards: %d \n", shards, brokenShards)
+	//	mgr, err1 := file.NewBlockMgr(dataShards, parShards)
+	//	if err1 != nil {
+	//		err = err1
+	//		return
+	//	}
+	//
+	//	wg := &sync.WaitGroup{}
+	//	wg.Add(shards - brokenShards)
+	//	wtrs := make([]io.Writer, shards)
+	//	for i := range rcs {
+	//		fh, err1 := ioutil.TempFile("", meta.FName+"."+strconv.Itoa(i))
+	//		if err1 != nil {
+	//			err = err1
+	//			return
+	//		}
+	//		if rcs[i] != nil {
+	//			go func() {
+	//				defer wg.Done()
+	//				_, err = io.Copy(fh, rcs[i])
+	//				if err != nil {
+	//					return
+	//				}
+	//			}()
+	//		} else {
+	//			wtrs[i] = fh
+	//		}
+	//	}
+	//	wg.Wait()
+	//	rds := make([]io.Reader, shards)
+	//	for i := range rcs {
+	//		if rcs[i] != nil {
+	//			rcs[i].Close()
+	//
+	//			fh, err1 := os.Open(meta.FName + "." + strconv.Itoa(i))
+	//			if err1 != nil {
+	//				err = err1
+	//				return
+	//			}
+	//			rds[i] = fh
+	//		} else {
+	//			rds[i] = nil
+	//		}
+	//	}
+	//	err = mgr.Reconstruct(rds, wtrs)
+	//	if err != nil {
+	//		return
+	//	}
+	//
+	//	for i := range rcs {
+	//		if rcs[i] == nil {
+	//			fh := wtrs[i].(*os.File)
+	//			fh.Seek(0, 0)
+	//			rcs[i] = fh
+	//		} else {
+	//			fh := rds[i].(*os.File)
+	//			fh.Seek(0, 0)
+	//			rcs[i] = fh
+	//		}
+	//
+	//		if i >= dataShards {
+	//			rcs[i].Close()
+	//		}
+	//	}
+	//
+	//	rcs = rcs[:dataShards]
+	//}
 
 	rc = utils.MultiReadCloser(rcs...)
 	return
@@ -557,8 +562,8 @@ func ReadAt(node *shell.Shell, fp string, offset, length int64) (rc io.ReadClose
 }
 
 // Remove the given path
-func Rm(s *shell.Shell, path string, recursive, force bool) error {
-	return s.Request("pin/add", path).
+func FilesRm(s *shell.Shell, path string, recursive, force bool) error {
+	return s.Request("files/rm", path).
 		Option("recursive", recursive).
 		Option("force", force).
 		Exec(context.Background(), nil)
