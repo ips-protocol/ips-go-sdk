62e1d8f275ce3853a96a450feef700b2d3edcfec
Jerry Bendy <jerry@icewingcc.com>
2019-07-31 18:08:00 +0800

    判断如果上传的视频是 h264 格式时，将不再进行转码

--- putPolicy/persistent/mediaConverter.go
+++ putPolicy/persistent/mediaConverter.go
@@ -6,6 +6,7 @@ import (
 	"github.com/ipweb-group/go-sdk/conf"
 	"github.com/ipweb-group/go-sdk/rpc"
 	"github.com/ipweb-group/go-sdk/utils"
+	"io"
 	"os"
 	"path"
 	"path/filepath"
@@ -17,55 +18,60 @@ import (
 func ConvertMediaJob() {
 	fmt.Println("[INFO] Convert media job is started")
 
-	rpcClient, err := rpc.NewClient(conf.GetConfig().NodeConf)
-	if err != nil {
-		panic(err)
-	}
-
 	// 每隔一段时间检查一次队列，并在有任务时执行任务
 	for {
 		time.Sleep(1 * time.Second)
-		convertMedia(rpcClient)
+		convertMedia()
 	}
 }
 
-func convertMedia(rpcClient *rpc.Client) {
+func convertMedia() {
 	// 获取第一个转换任务，并添加到转换中的 Hash 表中
 	task := GetFirstUnprocessedTask()
 	if task == nil {
 		return
 	}
 
-	fmt.Printf("[INFO] Convert task detected. Cid is %s, Ops is %s, NotifyUrl is %s \n", task.Cid, task.PersistentOps, task.PersistentNotifyUrl)
+	fmt.Printf("[INFO] Convert task detected. Hash is %s, Ops is %s, NotifyUrl is %s \n", task.Cid, task.PersistentOps, task.PersistentNotifyUrl)
+
+	// 是否需要转换，默认为需要。如果判断文件格式本身无需转换的话，将会设置此变量为 false
+	needConvert := true
+	var err error
 
 	// 有任务时，添加任务到正在转换的 Hash 表中
 	AddTaskToProcessingMap(task)
 
 	dir, filename, ext := parseFilePath(task.FilePath)
 
-	// TODO 判断如果视频是 h264 格式的话，将不再进行转换，而是直接回调成功
+	// 判断如果视频是 h264 格式的话，将不再进行转换，而是直接回调成功
+	if task.MediaInfo.Type == "h264" {
+		needConvert = false
+	}
 
 	// 启动转换任务
 	inputFilePath := task.FilePath
 	outputFilePath := dir + filename + "-converted" + ext
-	ffmpeg := conf.GetConfig().ExternalConfig.Ffmpeg
-	command := fmt.Sprintf("%s -stats -y -hide_banner -i %s -c:v libx264 -c:a libmp3lame %s", ffmpeg, inputFilePath, outputFilePath)
 
-	fmt.Printf("[INFO] Start ffmpeg converter, command is %s \n", command)
+	if needConvert {
+		ffmpeg := conf.GetConfig().ExternalConfig.Ffmpeg
+		command := fmt.Sprintf("%s -stats -y -hide_banner -i %s -c:v libx264 -c:a libmp3lame %s", ffmpeg, inputFilePath, outputFilePath)
+		fmt.Printf("[INFO] Start ffmpeg converter, command is %s \n", command)
 
-	result, err := utils.ExecCommand(command)
-	if err != nil {
-		// 转换失败后，把失败的任务插入到失败的 Hash 表中
-		fmt.Printf("[INFO] Convert task failed [%v] \n", err)
-		AddFailedTask(task, result)
-	} else {
-		fmt.Printf("%s \n", result)
-		fmt.Println("[INFO] Convert completed")
+		var result string // var 创建 result 变量，避免使用 := 时覆盖 err 变量的值
+		result, err = utils.ExecCommand(command)
+		if err != nil {
+			// 转换失败后，把失败的任务插入到失败的 Hash 表中
+			fmt.Printf("[INFO] Convert task failed [%v] \n", err)
+			AddFailedTask(task, result)
+		} else {
+			fmt.Printf("%s \n", result)
+			fmt.Println("[INFO] Convert completed")
+		}
 	}
 
 	// 转换成功后，上传转换后的文件到 IPFS
 	var dstCid string
-	if err == nil {
+	if err == nil && needConvert {
 		fmt.Printf("[INFO] Uploading converted file to IPFS")
 
 		file, err := os.Open(outputFilePath)
@@ -74,8 +80,10 @@ func convertMedia(rpcClient *rpc.Client) {
 			AddFailedTask(task, err.Error())
 
 		} else {
+			defer closeFile(file)
 			dstFileInfo, _ := file.Stat()
 
+			rpcClient, _ := rpc.GetClientInstance()
 			dstCid, err = rpcClient.Upload(file, dstFileInfo.Name(), dstFileInfo.Size())
 			if err != nil {
 				fmt.Printf("[ERROR] Upload converted file failed, [%v] \n", err)
@@ -87,30 +95,33 @@ func convertMedia(rpcClient *rpc.Client) {
 		}
 	}
 
-	// 删除临时文件
-	if err == nil {
-		_ = os.Remove(inputFilePath)
-		_ = os.Remove(outputFilePath)
-	}
+	// 删除临时文件（无论转换成功或者失败，都尝试删除文件）
+	_ = os.Remove(inputFilePath)
+	_ = os.Remove(outputFilePath)
 
 	// 移除 Redis 缓存
 	RemoveProcessingTask(task)
-	if err == nil {
+	if err == nil || !needConvert {
 		RemoveTask(task)
 	}
 
 	// 完成后回调（无论成功还是失败）
 	requestBody := NotifyRequestBody{
-		Cid:           task.Cid,
+		Hash:          task.Cid,
 		Code:          CodeSuccess,
 		Desc:          "",
 		PersistentOps: task.PersistentOps,
-		DstCid:        dstCid,
+		DstHash:       dstCid,
 	}
 
 	if err != nil {
 		requestBody.Code = CodeFailed
 		requestBody.Desc = err.Error()
+	}
+
+	// 如果无需转换，直接设置转换后的 hash 为原文件的 CID
+	if !needConvert {
+		requestBody.DstHash = task.Cid
 	}
 
 	stringContent, _ := json.Marshal(requestBody)
@@ -128,4 +139,12 @@ func parseFilePath(filePath string) (dir string, filename string, ext string) {
 	ext = path.Ext(base)
 	filename = strings.TrimSuffix(base, ext)
 	return
+}
+
+// 关闭文件。发生错误时不做任何处理
+func closeFile(file io.Closer) {
+	err := file.Close()
+	if err != nil {
+		fmt.Println("[WARN] An error occurred while closing file")
+	}
 }
--- putPolicy/persistent/persistentNotify.go
+++ putPolicy/persistent/persistentNotify.go
@@ -1,11 +1,11 @@
 package persistent
 
 type NotifyRequestBody struct {
-	Cid           string `json:"cid"`           // 原始文件的 CID
+	Hash          string `json:"hash"`          // 原始文件的 CID
 	Code          int    `json:"code"`          // 状态码。0 表示成功；1 表示失败
 	Desc          string `json:"desc"`          // 状态对应的描述
 	PersistentOps string `json:"persistentOps"` // 持久化操作类型
-	DstCid        string `json:"dstCid"`        // 持久化操作生成的目标文件的 CID
+	DstHash       string `json:"dstHash"`       // 持久化操作生成的目标文件的 CID
 }
 
 // 成功
