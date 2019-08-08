d26ad5917b158044893eaf3959f161a6d2ce5d43
rwifeng <rw.ifeng@gmail.com>
2019-08-08 16:20:20 +0800

    out put detail node info

--- websvr/nodes.go
+++ websvr/nodes.go
@@ -3,11 +3,6 @@ package websvr
 import "github.com/kataras/iris"
 
 func (s *Service) NodesList(ctx iris.Context) {
-	var ids []string
-	for id := range s.Node.Nodes {
-		ids = append(ids, id)
-	}
-
-	ctx.JSON(iris.Map{"ids": ids})
+	ctx.JSON(s.Node.Nodes)
 	return
 }
